ecc6b57912d301409186d92c0f7af9c42abe10e1
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-06-02 08:53:45 -0300

    Delete banco.py

--- banco.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-banco = sqlite3.connect('celulares.db')
-cursor = banco.cursor()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS celulares
-               (marca text, modelo text, valor text)''')
-
-
