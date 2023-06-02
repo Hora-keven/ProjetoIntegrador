487ef9a5ff134e76dd303294f3beb6b6a563adf0
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-06-02 08:52:16 -0300

    Add files via upload

---
+++ Python/main.py
@@ -0,0 +1,9 @@
+from tk_inter import Tela
+
+t = Tela()
+
+# /html/body/div[1]/div[1]/div/div[2]/div[3]/div[4]/a/div[2]/div[2]/div[1]/div /h2
+# /html/body/div[1]/div[1]/div/div[2]/div[3]/div[1]/a/div[2]/div[2]/div[1]/div/h2
+
+
+# /html/body/div[1]/div[1]/div/div[2]/div[3]/div[{i + 1}]/a/div[2]/div[2]/div[1]/div[1]/h2
