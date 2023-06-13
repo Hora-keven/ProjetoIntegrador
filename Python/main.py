a844602cab6dae73c9c638a76ee24fd313f3e499
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-06-13 10:38:20 -0300

    Delete Python directory

--- Python/main.py
+++
@@ -1,9 +0,0 @@
-from tk_inter import Tela
-
-t = Tela()
-
-# /html/body/div[1]/div[1]/div/div[2]/div[3]/div[4]/a/div[2]/div[2]/div[1]/div /h2
-# /html/body/div[1]/div[1]/div/div[2]/div[3]/div[1]/a/div[2]/div[2]/div[1]/div/h2
-
-
-# /html/body/div[1]/div[1]/div/div[2]/div[3]/div[{i + 1}]/a/div[2]/div[2]/div[1]/div[1]/h2
