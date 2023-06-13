2dcc585dbc6b6276f6c587b609f6cadf886c5aa5
Hora-keven <123791537+Hora-keven@users.noreply.github.com>
2023-06-13 10:41:21 -0300

    Create bot.py

---
+++ Python/bot.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from time import sleep
+import openpyxl
+
+
+class Bot:
+    def __init__(self):
+        self.navegador = webdriver.Chrome()
+        self.navegador.maximize_window()
+        self.abrir_site()
+        self.varrer_site()
+        self.excel()
+
+    def abrir_site(self):
+        self.navegador.get("file:///C:/Users/53688621808/Documents/PI/site/carrinho.html")
+
+    def varrer_site(self):
+        self.lista_valor = []
+        self.nome = []
+        sleep(5)
+        for i in range(16):
+            self.lista_valor.append(self.navegador.find_element(By.XPATH, f"/html/body/main/div/div[{i+1}]/div[1]").text)
+            self.nome.append(self.navegador.find_element(By.XPATH, f"/html/body/main/div/div[{i+1}]/div[2]").text)
+
+
+
+    def excel(self):
+        planilha = {'nome': self.nome,
+                    'valor': self.lista_valor}
+        data = pd.DataFrame(planilha)
+        data.to_excel('planilha_celulares.xlsx')
+        data.to_csv('planilha_celulares.csv', sep=";", index=False)
+
+
+t = Bot()
